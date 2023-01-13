c25e767e227945910f04f033fcddfd10aa629dc0
EC2 Default User <ec2-user@ip-172-31-81-223.ec2.internal>
2023-01-13 11:54:33 +0900

    失敗

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,7 +32,7 @@ class PostController extends Controller
        
     }
     
-    
+   
    
 }
 
--- database/migrations/2023_01_08_010822_create_posts_table.php
+++ database/migrations/2023_01_08_010822_create_posts_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('tatle', 50);
+            $table->string('body', 200);
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +29,11 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('posts');
+    }
+    
+    public function categoty()
+    {
+        return $this->belongsTo(Categoroy::class);
     }
 };
